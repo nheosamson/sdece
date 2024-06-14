c43a2274e19ed53dc8643a09e6cba54aaeeaa58b
NoketeX <rubens.castro@student.ateneo.edu>
2024-06-14 16:44:32 +0800

    Swapped panLocation and searchLocation Names; Moved onMapClick to index_UNIV

--- index_UNIV.js
+++ index_UNIV.js
@@ -29,21 +29,55 @@ export function getDivContent(name) {
     });
   }
  
-export function panLocation(name, map) {
+export function searchLocation(name, map) {
   console.log("PANNNNNN to " + name);   
   getDocIdByPartnerName(name).then((docId) => {
     getDocByID(docId).then((doc) => {
-      searchLocation(doc, map);
+      panLocation(doc, map);
     });
   });     
 }
 
-export function searchLocation(doc, map) {
+export function panLocation(doc, map) {
     console.log("Search location of "+ doc.id);
     doc = doc.data();
          console.log(typeof(map));   
     map.panTo(new L.LatLng(doc.location_latitude, doc.location_longitude));
-  }
+}
+
+
+
+// function onMapClick(e) {
+//   const lat = e.latlng.lat;
+//   const lng = e.latlng.lng;
+
+//   var popupContent = `
+//       <div class="partner-geolocation">
+//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+//             <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2ZM12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.66304 14.2366 10.2989 13.7678 10.7678C13.2989 11.2366 12.663 11.5 12 11.5Z" fill="#91C9DB"/>
+//             </svg>
+//             ${lat} + ${lng}
+//             <br>
+//         </div>
+//     <button class="addButton" data-lat="${lat}" data-lng="${lng}">Add Location</button>
+//   `;
+
+//   popup.setLatLng(e.latlng).setContent(popupContent).openOn(map);
+
+//   var addButton = document.querySelector(".addButton");
+//   addButton.addEventListener("click", function () {
+//     const lat = this.getAttribute("data-lat");
+//     const lng = this.getAttribute("data-lng");
+
+//     window.open(
+//       `addloc.html?lat=${encodeURIComponent(lat)}&lng=${encodeURIComponent(
+//         lng
+//       )}`,
+//       "_blank"
+//     );
+//   });
+// }
+// ////
 
 
 
@@ -70,3 +104,8 @@ export function readyField(field){
   field = capitalizeFirstLetters(field);
   return field;
 }
+
+
+
+
+
